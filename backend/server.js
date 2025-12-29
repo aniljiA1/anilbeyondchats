25b28e7c496329de68ef61ef105b3b8165ca9699
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2025-12-29 21:19:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ connectDB();
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
 
